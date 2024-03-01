37fac343acc6472ddc7d23e7204abfb9e89017a6
lukilampo <158604576+lukilampo@users.noreply.github.com>
2024-03-01 18:04:39 +0300

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,8 @@
+
+#1sololearn comparison operators -ის ჩათვლით
+#2მომხმარებელს შემოატანინეთ ორი რიცხვი, მისი ასაკი და მშობლის ასაკი, შემდეგ კი შეადარეთ ისინი ერთმანეთს
+
+you=int(input("how old are you?: "))
+mother=int(input("how old is your mother?: "))
+father=int(input("how old is your father?: "))
+print(you+mother+father)
